14f331a5809cf55c5493741f8f773e34b7e51924
Mahima <mahimamishra5008@gmail.com>
2025-08-29 18:35:53 +0530

    making api call from swiggy's api

--- FoodExpress/src/components/Body.js
+++ FoodExpress/src/components/Body.js
@@ -1,23 +1,48 @@
 import RestaurantCard from "./RestaurantCard"
-import restaurants from "../utils/mockData"
+// import restaurants from "../utils/mockData"
+import { useState, useEffect } from "react"
+import { API_URL } from "../utils/constant";
+
 
 
 //Body
 const Body = () => {
+
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+
+  }, [])
+  const fetchData = async () => {
+      const restData = await fetch(API_URL);
+      const jsonRestData = await restData.json();
+      setListOfRestaurants(jsonRestData.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }
+
+  const filterData = () => {
+    const data = listOfRestaurants.filter((restaurant) => {
+      return restaurant.info.avgRating >= 4.2;
+    })
+  // console.log("Button clicked")
+  setListOfRestaurants(data);
+  }
   return (
     <div className="body">
-      <div className="search">Search</div>
+
+      <button className="filter-btn" onClick={filterData}>Top Rated Restaurants</button>
+
       <div className="restaurant-container">
         {/* //Restaurant Card */}
-        {restaurants.map((restaurant) => {
+        {listOfRestaurants.map((restaurant) => {
           const { id,name, cuisines, avgRating, sla,cloudinaryImageId, aggregatedDiscountInfoV3} = restaurant.info;
           const resObj = {
             resName: name,
             cuisine: cuisines.join(', '),
             rating: avgRating,
-            time: sla?.deliveryTime || NA,
+            time: sla?.deliveryTime || "NA",
             imgId: cloudinaryImageId,
-            offer: (aggregatedDiscountInfoV3?.header || NA) + (aggregatedDiscountInfoV3?.subHeader || NA)
+            offer: (aggregatedDiscountInfoV3?.header || "NA") + (aggregatedDiscountInfoV3?.subHeader || "NA")
           }
             return < RestaurantCard key = { id } resData = { resObj } />
         })}  
